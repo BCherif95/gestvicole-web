dea6bd1442c683637a20dd90943dd617e1f8e8a1
Baba Maiga <babamaigafst@gmail.com>
2020-08-04 21:26:08 +0000

    Modification d'environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
     production: true,
-    hmr       : false
+    hmr       : false,
+    baseUrl: 'http://localhost:8080/gestion-avicole',
+    serviceUrl: 'http://localhost:8080/gestion-avicole/api',
+    /*serviceUrl: '/gestion-avicole/api',*/
+    authTitle: 'AUTHENTIFICATION',
+    errorMessage: 'Une erreur est survenue',
+    errorNetworkMessage: 'Echec de connexion au serveur',
+    notAuthorizedUserMessage: 'Vous n\'êtes pas autorisé à vous connecter sur cette application'
 };
